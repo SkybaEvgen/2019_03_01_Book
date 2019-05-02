c4a37b87f670d2ab17d96e8995641c686d16e032
SkybaEvgen <evgen.skyba@gmail.com.com>
2019-05-02 12:12:50 +0200

    first commit

---
+++ src/BookAppl.java
@@ -0,0 +1,29 @@
+
+public class BookAppl {
+
+	public static void main(String[] args) {
+		
+		Book book1 = new Book();
+		book1.setAuthor("Pushkin");
+		book1.setTitle("Onegin");
+		book1.setIsbn(123455);
+		book1.setYear(2001);
+		
+		Book book2=new Book();
+		book2.setAuthor("gogol");
+		book2.setTitle("revizor");
+		book2.setIsbn(12345);
+		book2.setYear(1567);
+		
+		book1.display();
+		
+		book2.display();
+		
+		System.out.println(book1.getAuthor());
+		System.out.println(book2.getTitle());
+		
+		String str3=book1.getTitle();
+		System.out.println(str3);
+	}
+
+}
